6b9af40726ebfe44d468437b0f1175bb632edd3f
shubham74041 <118410787+shubham74041@users.noreply.github.com>
2024-04-30 13:35:46 +0530

    Add files via upload

---
+++ mongo.js
@@ -0,0 +1,45 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+// const MONGO_URL = "mongodb://127.0.0.1:27017/project-data";
+const DBURL = process.env.ATLASDB;
+
+main()
+  .then(() => {
+    console.log("Connect to DB");
+  })
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(DBURL);
+}
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        // Regular expression to validate mobile numbers
+        return /^[0-9]{10}$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid mobile number!`,
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const cors = require("cors");
+const User = require("./mongo.js");
+
+const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/", cors(), (req, res) => {});
+
+app.post("/", async (req, res) => {
+  const { phoneNumber, password } = req.body;
+
+  try {
+    const data = await User.findOne({ phoneNumber: phoneNumber });
+    console.log(data);
+
+    if (data.password === password) {
+      res.json("exist");
+    } else {
+      res.json("notexist");
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//signup
+app.post("/signup", async (req, res) => {
+  const { email, phoneNumber, password } = req.body;
+
+  const data = {
+    email: email,
+    phoneNumber: phoneNumber,
+    password: password,
+  };
+  try {
+    const isExist = await User.findOne({ phoneNumber: phoneNumber });
+    console.log(isExist);
+    if (isExist) {
+      res.json("exists");
+    } else {
+      res.json("notexists");
+      await User.insertMany([data]);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.listen(8080, () => {
+  console.log("Listening on port 8080");
+});
