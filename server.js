8f585bf262c7c22cdabca41e34de9bdd5f7c17f6
Piyush <kumarpiyush2909@gmail.com>
2024-06-01 14:30:01 +0530

    feat/ update

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors());
+// app.use(cors());
 
 app.use(
   cors({
