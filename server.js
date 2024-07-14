bd8425cc4da8a0609c340954c3bb5b71309d6e96
Piyush <kumarpiyush2909@gmail.com>
2024-07-14 16:44:39 +0530

    feat/ add cors origin

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(bodyParser.json());
 
 app.use(
   cors({
-    origin: "https://finance-king-pi.vercel.app", // Replace with your frontend's domain
+    origin: "http://43.204.37.41", // Replace with your frontend's domain
     methods: "GET,POST", // Specify the allowed methods
     credentials: true, // Allow credentials if needed
   })
