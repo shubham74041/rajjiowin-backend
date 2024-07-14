8822e3e3a955028ddfdaaa2db02d7593d4f3760f
shubham74041 <shubham.in0181@gmail.com>
2024-07-14 21:51:04 +0530

    url change

--- server.js
+++ server.js
@@ -28,7 +28,9 @@ app.use(bodyParser.json());
 
 app.use(
   cors({
-    origin: "https://finance-king-pi.vercel.app", // Replace with your frontend's domain
+    // origin: "https://finance-king-pi.vercel.app", // Replace with your frontend's domain
+    origin: "http://65.2.142.221", // Replace with your frontend's domain
+    
     methods: "GET,POST", // Specify the allowed methods
     credentials: true, // Allow credentials if needed
   })
