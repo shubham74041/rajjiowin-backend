b547c5afc7c4a537780a47def3e2775690c18950
Piyush <kumarpiyush2909@gmail.com>
2024-07-24 16:34:33 +0530

    ip add

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(bodyParser.json());
 app.use(
   cors({
     // origin: "https://finance-king-pi.vercel.app", // Replace with your frontend's domain
-    origin: "http://rajjowin.in", // Replace with your frontend's domain
+    origin: "http://43.204.109.203", // Replace with your frontend's domain
 
     methods: "GET,POST", // Specify the allowed methods
     credentials: true, // Allow credentials if needed
